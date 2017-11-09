c5ff0a0f7296aa98ca8cbbc95b99d70b37284d4c
Anqi Pan <anqipan@usc.edu>
2017-11-08 19:42:51 -0800

    Diamonds App

---
+++ diamondsViz/Untitled.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(ggplot2)
+
+# user interface
+
+ui = fluidPage(
+  titlePanel("Diamonds Data",windowTitle = "Diamonds"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("This app is to visulize diamonds dataset"),
+      textInput(inputId = "title", label = "Chart Title", value = ""),
+      
+    selectInput(inputId = "pos",
+                label = "Position",
+                choices = list("stack","dodge"),
+                selected = "stack")
+    ),
+    
+
+    mainPanel(
+      plotOutput(outputId = "plot")
+    )
+  )
+  
+)
+
+# server
+
+server = function(input, output){
+  output$plot = renderPlot(
+  ggplot(diamonds, aes(x=cut,fill = clarity))+
+    geom_bar(position = input$pos)+
+    ggtitle(input$title)
+  )
+  
+}
+
+# run the app
+shinyApp(ui,server)
+
+# side bar panel <- ######  -> main panel
+
+
+
+
+
+
+
+
+
+
+
+
